801751df96b49fdd579ce428a2372c0fa4eba5a1
Julia <juliatuvaleva@mail.ru>
2021-04-01 16:12:58 +0300

    project updated with new classes

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -34,19 +34,14 @@ export const validationSettings = {
   inputErrorClass: 'popup__form-input_type_error',
   errorClass: 'popup__form-error_active'
 };
-
-export const profileName = document.querySelector('.profile__name');
-export const profileBio = document.querySelector('.profile__bio');
 export const editBtn = document.querySelector('.profile__edtn-button');
 export const addBtn = document.querySelector('.profile__add-button');
-const popupCard = document.querySelector('.popup_type_cards');
 export const popupImage = '.popup_type_image';
 export const popupImageContent = document.querySelector('.popup__image-content');
 export const popupImageCaption = document.querySelector('.popup__image-caption');
 export const nameInput = document.querySelector('.popup__form-input_field_name');
 export const jobInput = document.querySelector('.popup__form-input_field_bio');
-export const cardNameInput = document.querySelector('.popup__form-input_field_image-name');
-export const cardLinkInput = document.querySelector('.popup__form-input_field_image-src');
+const popupCard = document.querySelector('.popup_type_cards');
 export const saveBtn = popupCard.querySelector('.popup__submit-btn');
 export const cardForm = document.forms['card-form'];
 export const profileForm = document.forms['profile-form'];
